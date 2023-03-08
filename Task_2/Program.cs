052e9e4548c73a05c0afcfc50c2d32cbbc210cbf
Mullery228 <102748044+Mullery228@users.noreply.github.com>
2023-03-08 19:29:45 +0700

    Add programm's

---
+++ Task_2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите координаты x, y, z для точки А:\n");
+double x1 = Convert.ToDouble(Console.ReadLine());
+double y1 = Convert.ToDouble(Console.ReadLine());
+double z1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите координаты x, y, z для точки B:\n");
+double x2 = Convert.ToDouble(Console.ReadLine());
+double y2 = Convert.ToDouble(Console.ReadLine());
+double z2 = Convert.ToDouble(Console.ReadLine());
+
+double a = 0;
+a = Math.Pow((Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2)), 0.5);
+Console.WriteLine(a);
---
+++ Task_3/Program.cs
@@ -0,0 +1,6 @@
+﻿int N = Convert.ToInt32(Console.ReadLine());
+
+for(int i = 1; i <= N; i++)
+{
+    Console.WriteLine(Math.Pow(i, 3));
+}
